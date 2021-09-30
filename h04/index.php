93a7062da6f64cf5c0fd1e40bde521c1ab618b0d
MrPotato-04 <80412415+MrPotato-04@users.noreply.github.com>
2021-09-30 09:50:29 +0200

    h04 af

--- h04/index.php
+++ h04/index.php
@@ -3,4 +3,31 @@ function blankLine() {
     echo nl2br("\n");
 };
 
+function tempConvert($cels) {
+    $fahr = ($cels * 9.0/5.0) + 32.0;
+    return $fahr;
+};
+echo tempConvert(22);
+
+function devide3($num) {
+    if($num % 3 === 0) {
+        return $num . " is deelbaar door 3";
+    } else {
+        return $num . " is niet deelbaar door 3";
+    };
+};
+blankLine();
+
+echo devide3(7);
+
+blankLine();
+
+function reverseString($args) {
+    $out = strrev($args);
+    return $out;
+}
+
+echo reverseString("racecar");
+
+blankLine();
 
